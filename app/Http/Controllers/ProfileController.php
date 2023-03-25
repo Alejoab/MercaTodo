52fe5829b180d34eecfd764ca8a57f8991acabe9
Alejoab <aalvarezbo@unal.edu.co>
2023-03-24 22:30:43 -0500

    feat: create the user profile update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Department;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -21,6 +22,8 @@ class ProfileController extends Controller
         return Inertia::render('Profile/Edit', [
             'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
             'status' => session('status'),
+            'departments' => Department::all(),
+            'department_id' => $request->user()->city->department->id,
         ]);
     }
 
@@ -35,6 +38,18 @@ class ProfileController extends Controller
             $request->user()->email_verified_at = null;
         }
 
+        $request->user()->save();
+
+        return Redirect::route('profile.edit');
+    }
+
+    /**
+     * Update the user's address information.
+     */
+    public function updateAddress(Request $request): RedirectResponse
+    {
+        $request->user()->city_id = $request->city_id;
+        $request->user()->address = $request->address;
         $request->user()->save();
 
         return Redirect::route('profile.edit');
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile-address', [ProfileController::class, 'updateAddress'])->name('profile.update.address');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
