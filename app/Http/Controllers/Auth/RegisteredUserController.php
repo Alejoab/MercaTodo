a6fbce502a450ab3997f3d91deddc808a507a803
Alejoab <aalvarezbo@unal.edu.co>
2023-04-10 22:01:19 -0500

    feat: modify the customer registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,60 +4,44 @@ namespace App\Http\Controllers\Auth;
 
 use App\Enums\DocumentType;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CustomerRequest;
+use App\Models\Customer;
 use App\Models\Department;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class RegisteredUserController extends Controller
 {
     /**
-     * Display the registration view.
-     */
-    public function create(): Response
-    {
-        return Inertia::render('Auth/Register', [
-            'document_types' => DocumentType::cases(),
-            'departments' => Department::all(),
-        ]);
-    }
-
-    /**
      * Handle an incoming registration request.
      *
-     * @throws \Illuminate\Validation\ValidationException
+     * @throws ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(CustomerRequest $request): RedirectResponse
     {
+        $validated = $request->validated();
 
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'surname' => 'required|string|max:255',
-            'document' => 'required|digits_between:8,11|unique:'.User::class,
-            'document_type' => new Rules\Enum(DocumentType::class),
-            'email' => 'required|string|email|max:255|unique:'.User::class,
-            'phone' => 'nullable|digits:10',
-            'city_id' => 'required|exists:cities,id',
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        $customer = Customer::create([
+            'document' => $validated['document'],
+            'document_type' => $validated['document_type'],
+            'phone' => $validated['phone'],
+            'address' => $validated['address'],
+            'city_id' => $validated['city_id'],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
-            'surname' => $request->surname,
-            'document' => $request->document,
-            'document_type' => $request->document_type,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'address' => $request->address,
-            'city_id' => $request->city_id,
-            'password' => Hash::make($request->password),
+            'name' => $validated['name'],
+            'surname' => $validated['surname'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+            'customer_id' => $customer->id,
         ]);
 
         $user->assignRole('Customer');
@@ -67,5 +51,16 @@ class RegisteredUserController extends Controller
         Auth::login($user);
 
         return redirect(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * Display the registration view.
+     */
+    public function create(): Response
+    {
+        return Inertia::render('Auth/Register', [
+            'document_types' => DocumentType::cases(),
+            'departments' => Department::all(),
+        ]);
     }
 }
