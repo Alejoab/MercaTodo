0e2ede5ed0e85043da656f7922af6ac38a2125a6
Alejoab <aalvarezbo@unal.edu.co>
2023-04-19 07:54:36 -0500

    feat: change design of navigation bars

---
+++ resources/js/Components/GuestNavBar.vue
@@ -0,0 +1,88 @@
+<script setup>
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import {Link} from "@inertiajs/vue3";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import {ref} from "vue";
+
+const showingNavigationDropdown = ref(false);
+</script>
+
+<template>
+    <nav class="bg-white border-b border-gray-100">
+        <!-- Primary Navigation Menu -->
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="shrink-0 flex items-center">
+                        <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800"/>
+                    </div>
+                </div>
+
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <div class="ml-3 relative">
+                        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                            <Link
+                                :href="route('login')"
+                                class="font-semibold text-gray-400 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-100"
+                            >Log in
+                            </Link>
+
+                            <Link
+                                :href="route('register')"
+                                class="ml-4 font-semibold text-gray-400 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-100"
+                            >Register
+                            </Link
+                            >
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Hamburger -->
+                <div class="-mr-2 flex items-center sm:hidden">
+                    <button
+                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                        @click="showingNavigationDropdown = !showingNavigationDropdown"
+                    >
+                        <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path
+                                :class="{
+                                            hidden: showingNavigationDropdown,
+                                            'inline-flex': !showingNavigationDropdown,
+                                        }"
+                                d="M4 6h16M4 12h16M4 18h16"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                            />
+                            <path
+                                :class="{
+                                            hidden: !showingNavigationDropdown,
+                                            'inline-flex': showingNavigationDropdown,
+                                        }"
+                                d="M6 18L18 6M6 6l12 12"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Responsive Navigation Menu -->
+        <div
+            :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
+            class="sm:hidden"
+        >
+            <!-- Responsive Settings Options -->
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                <div class="mt-3 space-y-1">
+                    <ResponsiveNavLink :href="route('register')"> Register</ResponsiveNavLink>
+                    <ResponsiveNavLink :href="route('login')"> Log In</ResponsiveNavLink>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,19 +1,12 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import {Link} from '@inertiajs/vue3';
+import GuestNavBar from "@/Components/GuestNavBar.vue";
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-                <ApplicationLogo/>
-            </Link>
-        </div>
+    <div class="min-h-screen bg-gray-100">
+        <GuestNavBar/>
 
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
-        >
+        <div class="w-full sm:max-w-md mt-12 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg m-auto">
             <slot/>
         </div>
     </div>
