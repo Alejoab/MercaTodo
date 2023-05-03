def60485ccb4b44b4f649973a75ddf7ab3cf7b70
Alejoab <aalvarezbo@unal.edu.co>
2023-05-02 22:17:15 -0500

    feat: add cache to cities and departments list

--- app/Services/Customers/CitiesService.php
+++ app/Services/Customers/CitiesService.php
@@ -5,16 +5,21 @@ namespace App\Services\Customers;
 use App\Models\City;
 use App\Models\Department;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class CitiesService
 {
     public function citiesByDepartment(int $department_id): Collection|array
     {
-        return City::query()->where('department_id', $department_id)->get();
+        return Cache::rememberForever('cities_by_department_'.$department_id, function () use ($department_id) {
+            return City::query()->where('department_id', $department_id)->get();
+        });
     }
 
     public function departments(): Collection
     {
-        return Department::all();
+        return Cache::rememberForever('departments', function () {
+            return Department::all();
+        });
     }
 }
