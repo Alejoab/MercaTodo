f69a290a097e4f5f3e9dda6558998a7733412f6e
Alejoab <aalvarezbo@unal.edu.co>
2023-06-11 15:28:29 -0500

    feat: delete expiration link variable in .env

--- config/placetopay.php
+++ config/placetopay.php
@@ -4,5 +4,5 @@ return [
     'login' => env('PLACETOPAY_LOGIN'),
     'tranKey' => env('PLACETOPAY_TRANKEY'),
     'url' => env('PLACETOPAY_URL'),
-    'expirationLink' => env('PLACETOPAY_EXPIRATION_LINK'),
+    'expirationLink' => 6,
 ];
