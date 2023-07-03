2abac1987287144bf1e7c85441c8bab472dd39cc
Alejoab <aalvarezbo@unal.edu.co>
2023-07-03 17:34:00 -0500

    feat: create action 'RestoreProducts'

--- app/Actions/Orders/RejectOrderAction.php
+++ app/Actions/Orders/RejectOrderAction.php
@@ -26,21 +26,7 @@ class RejectOrderAction implements RejectOrder
                 return;
             }
 
-            foreach ($order->order_detail as $order_detail) {
-                /**
-                 * @var Order_detail $order_detail
-                 */
-                if (!$order_detail->product) {
-                    continue;
-                }
-
-                if ($order_detail->product->deleted_at !== null) {
-                    $order_detail->product->restore();
-                }
-
-                $order_detail->product->stock += $order_detail->quantity;
-                $order_detail->product->save();
-            }
+            (new RestoreProductsAction())->execute($order);
 
             $order->active = false;
             $order->save();
