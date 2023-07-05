165cea46da614edcb83644c4ff85b16600eafb6d
Alejoab <aalvarezbo@unal.edu.co>
2023-07-04 21:29:29 -0500

    feat: create exports disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,6 +42,12 @@ return [
             'throw' => false,
         ],
 
+        'exports' => [
+            'driver' => 'local',
+            'root' => storage_path('app/exports'),
+            'throw' => false,
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
