9f05f417f64e09902e0909e0c7ae4a6466087b96
Alejoab <aalvarezbo@unal.edu.co>
2023-07-11 14:45:22 -0500

    feat: create reports type enum

--- app/Domain/Reports/Enums/ReportType.php
+++ app/Domain/Reports/Enums/ReportType.php
@@ -2,7 +2,11 @@
 
 namespace App\Domain\Reports\Enums;
 
-enum ReportEnum
+enum ReportType: string
 {
-
+    case R1 = 'Sales by category';
+    case R2 = 'Sales by brand';
+    case R3 = 'Sales by product';
+    case R4 = 'Sales by payment method';
+    case R5 = 'Sales by department';
 }
